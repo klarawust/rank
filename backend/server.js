bccfb9f9806e47f35bc12d4de6a1e759455b2cb3
Klara Wüstenberg <klarawust@gmail.com>
2023-11-16 22:25:53 +0200

    Make server and endpoints for /createGame and /joinGame

--- backend/server.js
+++ backend/server.js
@@ -5,17 +5,60 @@ const app = express();
 const jsonParser = bodyParser.json();
 const port = 8080;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const games = [];
+
+const generateGameId = () => {
+  return Math.floor(Math.random() * (99999 - 10000) + 10000);
+};
+
+//endepunkt createGame
+app.post("/createGame", jsonParser, (req, res) => {
+  const username = req.body.username;
+  const gameId = generateGameId();
+
+  const game = {
+    gameId: gameId,
+    creator: username,
+    players: [],
+  };
+  game.players.push(username);
+  games.push(game);
+
+  res.json({
+    message:
+      "Game created and added to list of games, creator of game added as a player.",
+    newGame: game,
+    games: games,
+  });
 });
 
-app.get("/isak", (req, res) => {
-  res.send("Hello Isak!");
-});
+//endepunkt joinGame
+app.post("/joinGame", jsonParser, (req, res) => {
+  const username = req.body.username;
+  const gameIdInput = req.body.gameId;
 
-app.post("/", jsonParser, (req, res) => {
-  console.log(req.body.best);
-  res.send("Hello!");
+  let gameFound = false;
+
+  for (let i = 0; i < games.length; i++) {
+    if (games[i].gameId.toString() === gameIdInput) {
+      games[i].players.push(username);
+      gameFound = true;
+      break;
+    }
+  }
+  console.log({ games });
+
+  if (gameFound) {
+    res.json({
+      message: "Successfully added user to game",
+      games: games,
+    });
+  } else {
+    res.json({
+      message: "Game does not exist",
+      games: games,
+    });
+  }
 });
 
 app.listen(port, () => {
